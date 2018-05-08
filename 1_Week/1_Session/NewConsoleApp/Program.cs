400241a07fbd58eee88a23a73256408d00168a0a
WorldWideDev <dev206@gmail.com>
2018-05-07 20:28:09 -0700

    first lecture code

---
+++ 1_Week/1_Session/NewConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NewConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbys = new int[]
+            {
+                234,2345,23,13,13445,2345,1,-124
+            };
+
+            int smallestNumby = MinNum(numbys);
+
+            Console.WriteLine(smallestNumby);
+            
+        }
+
+        static int MinNum(int[] numbers)
+        {
+            // find the lowest one!
+            int currMin = numbers[0];
+
+            // iterate through the array
+            for(int i = 0; i<numbers.Length; i++)
+            {
+                if(currMin > numbers[i])
+                    currMin = numbers[i];
+            }
+            return currMin;
+        }
+    }
+}
---
+++ 1_Week/1_Session/NewConsoleApp/test.cs.lol
@@ -0,0 +1,16 @@
+
+    // 32bit Integer
+    int myInt = 5;
+    double myDouble = 1034.231213;
+    string myString = "five";
+    bool isFive = true;
+
+    int converted = (int)myDouble;
+
+
+    int SomeFunction(int number)
+    {
+        return number * 5;
+    }
+
+    int valueFromFunction = SomeFunction(5);
